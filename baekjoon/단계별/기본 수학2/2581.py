d325df22f3e902df1ded5b7cba2935e980b2a187
origin <0219alice@gmail.com>
2022-04-19 19:23:40 +0900

    [baekjoon] 소수

---
+++ baekjoon/단계별/기본 수학2/2581.py
@@ -0,0 +1,14 @@
+import math, sys
+
+m, n = map(int, sys.stdin.readline().split())
+arr = [True for i in range(n + 1)]
+
+for i in range(2, int(math.sqrt(n)) + 1):
+    if arr[i]:
+        for j in range(i + i, n + 1, i):
+            arr[j] = False
+
+for i in range(m, n + 1):
+    if i > 1 and arr[i]:
+        print(i)
+
