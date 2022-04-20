e73a088624c636cfa34d0540333ca81e92988fab
origin <0219alice@gmail.com>
2022-04-20 23:56:27 +0900

    [baekjoon] 좌표 정렬하기

---
+++ baekjoon/단계별/정렬/11650.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+arr = []
+
+for _ in range(int(input())):
+    x, y = map(int, input().split())
+    arr.append((x, y))
+
+arr.sort(key=lambda x:(x[0], x[1]))
+
+for i in arr:
+    print(i[0], i[1])
