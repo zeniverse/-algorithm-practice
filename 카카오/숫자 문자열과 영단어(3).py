de5016a7ed46fbe7bc9aef33643cebe41f873ec8
origin <0219alice@gmail.com>
2022-05-21 16:20:45 +0900

    [programmers] 숫자 문자열과 영단어(3)

---
+++ 카카오/숫자 문자열과 영단어(3).py
@@ -0,0 +1,8 @@
+s = "2three45sixseven"
+num_dict = {"zero" : '0', "one" : '1', "two" : '2', "three" : '3', "four" : '4', "five" : '5', "six" : '6', "seven" : '7', "eight" : '8', "nine" : '9'}
+res = s
+
+for k, v in num_dict.items():
+    res = res.replace(k, v)
+
+print(res)
