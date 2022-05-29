e91b6b54837a29689eddf615f37c022651d6d542
origin <0219alice@gmail.com>
2022-05-29 10:41:59 +0900

    [baekjoon] 최소공배수(1)

---
+++ baekjoon/수학/1934.py
@@ -0,0 +1,10 @@
+
+def gcd(a, b):
+    if b == 0:
+        return a
+    return gcd(b, a % b)
+
+for _ in range(int(input())):
+    a, b = map(int, input().split())
+
+    print((a * b) // gcd(a, b))
