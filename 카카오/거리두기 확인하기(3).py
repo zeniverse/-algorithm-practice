2fafac472ca313f3542adf4b1b8328c46a9131c3
origin <0219alice@gmail.com>
2022-06-09 15:37:03 +0900

    [programmers] 거리두기 확인하기(3)

---
+++ 카카오/거리두기 확인하기(3).py
@@ -0,0 +1,25 @@
+places = [["POOOP", "OXXOX", "OPXPX", "OOXOX", "POXXP"], ["POOPX", "OXPXP", "PXXXO", "OXXXO", "OOOPP"], ["PXOPX", "OXOXP", "OXPOX", "OXXOP", "PXPOX"], ["OOOXX", "XOOOX", "OOOXX", "OXOOX", "OOOOO"], ["PXPXP", "XPXPX", "PXPXP", "XPXPX", "PXPXP"]]
+
+def check(place):
+    for irow, row in enumerate(place):
+        for icol, cell in enumerate(row):
+            if cell != 'P':
+                continue
+
+            if irow != 4 and place[irow + 1][icol] == 'P':
+                return 0
+            if icol != 4 and place[irow][icol + 1] == 'P':
+                return 0
+
+            if irow < 3 and place[irow + 2][icol] == 'P' and place[irow + 1][icol] != 'X':
+                return 0
+            if icol < 3 and place[irow][icol + 2] == 'P' and place[irow][icol + 1] != 'X':
+                return 0
+            
+            if irow != 4 and icol != 4 and place[irow + 1][icol + 1] == 'P' and (place[irow + 1][icol] != 'X' or place[irow][icol + 1] != 'X'):
+                return 0
+            if irow != 4 and icol != 0 and place[irow + 1][icol - 1] == 'P' and (place[irow + 1][icol] != 'X' or place[irow][icol - 1] != 'X'):
+                return 0
+    return 1
+
+print([check(place) for place in places])
