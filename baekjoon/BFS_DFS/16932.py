1b9bf2bf80e8c280ad7843f01b08a964a553210a
origin <0219alice@gmail.com>
2022-06-11 13:20:22 +0900

    [baekjoon] 모양 만들기

--- baekjoon/BFS_DFS/16932.py
+++ baekjoon/BFS_DFS/16932.py
@@ -1,58 +1,75 @@
-from sys import stdin
 from collections import deque
-input = stdin.readline
+import sys
+input = sys.stdin.readline
 
-N, M = map(int, input().split())
-graph = []
-for _ in range(N):
-    graph.append(list(map(int, input().split())))
-visited = [[0] * M for _ in range(N)]
+dx = [0, 0, 1, -1]
+dy = [1, -1, 0, 0]
 
-dy = [-1, 1, 0, 0]
-dx = [0, 0, -1, 1]
-def bfs(i, j):
-    cnt = 1
-    q = deque()
-    q.append((i, j))
-    visited[i][j] = num
-    while q:
-        y, x = q.popleft()
-        for l in range(4):
-            yy = dy[l] + y
-            xx = dx[l] + x
-            if 0 <= yy < N and 0 <= xx < M:
-                if not visited[yy][xx] and graph[yy][xx] == 1:
-                    visited[yy][xx] = num
-                    q.append((yy, xx))
-                    cnt += 1
+# '1'로 묶어주는 BFS
+def bfs(x, y):
+    queue = deque()
+    queue.append((x, y))
+
+    visited[x][y] = num
+    cnt = 0
+
+    while queue:
+        x, y = queue.popleft()
+        cnt += 1
+
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if nx < 0 or nx >= n or ny < 0 or ny >= m:
+                continue
+
+            if not visited[nx][ny] and arr[nx][ny] == 1:
+                visited[nx][ny] = num
+                queue.append((nx, ny))
+
     return cnt
 
-# '1' 그룹화 진행 -> num은 2부터 시작
-num = 1
-group = dict()
-for i in range(N):
-    for j in range(M):
-        if graph[i][j] == 1 and not visited[i][j]:
-            cnt = bfs(i, j)
-            group[num] = cnt
-            num += 1
-print(group)
-print(visited)
 
-result = 0
-for y in range(N):
-    for x in range(M):
-        if graph[y][x] == 0:
-            s = set()
-            for l in range(4):
-                yy = dy[l] + y
-                xx = dx[l] + x
-                if 0 <= yy < N and 0 <= xx < M:
-                    if graph[yy][xx] == 1 and visited[yy][xx] not in s:
-                        s.add(visited[yy][xx])
-            res = 1
-            print("test", s)
-            for ss in s:
-                res += group[ss]
-            result = max(result, res)
-print(result)
+n, m = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(n)]
+
+num = 1
+count_dict = {}
+visited = [[0] * m for _ in range(n)]
+
+for i in range(n):
+    for j in range(m):
+        if arr[i][j] == 1 and not visited[i][j]:
+            count = bfs(i, j)
+            count_dict[num] = count
+            num += 1
+            
+
+res = 0
+
+for i in range(n):
+    for j in range(m):
+        if arr[i][j] == 0:
+            tmp = set()
+            # '0'에서 '1'로 바꿀때 본인을 이미 더해서 sum이 1로 시작한다
+            sum = 1 
+            
+            for k in range(4):
+                xx = i + dx[k]
+                yy = j + dy[k]
+
+                if xx < 0 or xx >= n or yy < 0 or yy >= m:
+                    continue
+                
+                # 근접한 곳에 '1'이 있다면?
+                if arr[xx][yy] != 0 and visited[xx][yy] not in tmp:
+                    tmp.add(visited[xx][yy])
+
+            # 근접한 '1'의 개수를 다 더하기
+            # ex) count_dict[1] = 3 + count_dict[2] = 1
+            for l in tmp:
+                sum += count_dict[l]
+
+            res = max(res, sum)
+print(res)
