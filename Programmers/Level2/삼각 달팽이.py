fd74cdd2854fdbc8d19bb04dedce47bebb624e24
origin <0219alice@gmail.com>
2022-08-31 20:20:10 +0900

    [programmers] 삼각 달팽이

---
+++ Programmers/Level2/삼각 달팽이.py
@@ -0,0 +1,28 @@
+
+from ast import Num
+
+
+n = 4
+
+res = [[0 for j in range(1, i + 1)] for i in range(1, n + 1)]
+print(res)
+
+x, y = -1, 0
+num = 1
+
+
+for i in range(n):
+    for j in range(i, n):
+        if i % 3 == 0:
+            x += 1
+        elif i % 3 == 1:
+            y += 1
+        else:
+            x -= 1
+            y -= 1
+
+        res[x][y] = num
+        num += 1
+
+print(sum(res, []))
+        
