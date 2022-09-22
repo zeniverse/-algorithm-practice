1c891c0a8b600d8cc2767f0cf14f77b833fa8e5b
origin <0219alice@gmail.com>
2022-09-22 16:13:32 +0900

    [baekjoon] 토마토

---
+++ baekjoon/BFS_DFS/7576(3).py
@@ -0,0 +1,43 @@
+from collections import deque
+import sys
+
+dx = [0, 0, 1, -1]
+dy = [1, -1, 0, 0]
+
+def bfs():
+    day = -1
+
+    while queue:
+        day += 1
+
+        for _ in range(len(queue)):
+            x, y = queue.popleft()
+
+            for i in range(4):
+                nx = x + dx[i]
+                ny = y + dy[i]
+
+                if nx < 0 or nx >= n or ny < 0 or ny >= m:
+                    continue
+                
+                if arr[nx][ny] == 0:
+                    arr[nx][ny] = arr[x][y] + 1
+                    queue.append((nx, ny))
+
+    for i in arr:
+        if 0 in i:
+            return -1
+
+    return day
+
+
+m, n = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(n)]
+queue = deque()
+
+for i in range(n):
+    for j in range(m):
+        if arr[i][j] == 1:
+            queue.append((i, j))
+
+print(bfs())
