6ec8206d019d59cfc0f737f931ad37c6a59de5f3
origin <0219alice@gmail.com>
2022-11-03 16:29:33 +0900

    [baekjoon] N과 M (4)

---
+++ baekjoon/백트래킹/15652.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+arr = [0] * m
+
+def dfs(k):
+    if k == m:
+        print(*arr)
+        return
+
+    for i in range(1, n + 1):
+        if arr[k - 1] <= i:
+            arr[k] = i
+            dfs(k + 1)
+            arr[k] = 0
+
+dfs(0)
