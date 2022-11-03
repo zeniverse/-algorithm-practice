edf800988f961702e72e1ed4d60501839ddbec8f
origin <0219alice@gmail.com>
2022-11-03 16:21:11 +0900

    [baekjoon] N과 M (2)

---
+++ baekjoon/백트래킹/15650.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+arr = [0] * m
+visited = [False] * (n + 1)
+
+def dfs(k):
+    if k == m:
+        print(*arr)
+        return
+    
+    for i in range(1, n + 1):
+        if not visited[i]:
+            if arr[k - 1] < i:
+                arr[k] = i
+                visited[i] = True
+                dfs(k + 1)
+                arr[k] = 0
+                visited[i] = False
+
+dfs(0)
