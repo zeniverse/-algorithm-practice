131d620971e393fa235b347886a36f074a19e491
origin <0219alice@gmail.com>
2022-11-04 22:40:32 +0900

    [baekjoon] 단어 정렬

---
+++ baekjoon/정렬/1181.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+arr = [input().rstrip() for _ in range(n)]
+
+arr = list(set(arr))
+arr.sort(key=lambda x:(len(x), x))
+
+for i in arr:
+    print(i)
