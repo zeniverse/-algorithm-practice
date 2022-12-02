beec9311e7306ecc423cae506df3e6af373a4e7f
origin <0219alice@gmail.com>
2022-12-02 22:45:20 +0900

    [programmers] 문자열 정렬하기(1)

---
+++ Programmers/Level0/문자열 정렬하기(1).py
@@ -0,0 +1,13 @@
+my_string = "hi12392"
+
+def solution(my_string):
+    answer = []
+
+    for i in my_string:
+        if i.isdigit():
+            answer.append(int(i))
+
+    answer.sort()
+    return answer
+
+print(solution(my_string))
