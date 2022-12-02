8b56f97d8fb273a9c87ff28e18fc8bb75e682ebc
origin <0219alice@gmail.com>
2022-12-02 22:24:21 +0900

    [programmers] 옷가게 할인 받기

---
+++ Programmers/Level0/옷가게 할인 받기.py
@@ -0,0 +1,15 @@
+price = 580000
+
+def solution(price):
+    discount = 0
+
+    if price >= 500000:
+        discount = 0.2
+    elif price >= 300000:
+        discount = 0.1
+    elif price >= 100000:
+        discount = 0.05
+
+    return int(price - (price * discount))
+
+print(solution(price))
