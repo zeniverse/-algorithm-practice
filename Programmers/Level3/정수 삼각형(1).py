46041c1e37a35c3d88cba2db776d8711c2e91ff1
origin <0219alice@gmail.com>
2023-02-13 10:43:31 +0900

    [programmers] 정수 삼각형(1)

--- Programmers/Level3/정수 삼각형(1).py
+++ Programmers/Level3/정수 삼각형(1).py
@@ -1,15 +1,17 @@
 
+
 def solution(triangle):
     dp = [[0] * len(triangle) for _ in range(len(triangle))]
+
     dp[0][0] = triangle[0][0]
 
-    for i in range(0, len(triangle) - 1):
+    for i in range(len(triangle) - 1):
         for j in range(len(triangle[i])):
             dp[i + 1][j] = max(dp[i + 1][j], dp[i][j] + triangle[i + 1][j])
             dp[i + 1][j + 1] = max(dp[i + 1][j + 1], dp[i][j] + triangle[i + 1][j + 1])
 
-
     return max(dp[-1])
+    
 
 triangle = [[7], [3, 8], [8, 1, 0], [2, 7, 4, 4], [4, 5, 2, 6, 5]]
 print(solution(triangle))
