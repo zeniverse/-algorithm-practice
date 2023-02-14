303a7e653245d6d25ac6ee68e0191af1f0220640
origin <0219alice@gmail.com>
2023-02-14 09:18:44 +0900

    [programmers] 최고의 집합

--- Programmers/Level3/최고의 집합.py
+++ Programmers/Level3/최고의 집합.py
@@ -1,16 +1,25 @@
+
+
 def solution(n, s):
-    answer = []
-
-    if s < n :
+    if n > s:
         return [-1]
+    
+    # 곱이 가장 큰 합을 만드려면,
+    # 공평하게 모두 나눈 후에 나머지를 각각 1씩 더해주면 된다.
+    # ex) n = 3, s = 8 일때, 8을 3으로 나누면 몫은 2 나머지도 2 가된다 
+    # res = [2, 2, 2] 를 만들어주고 나머지 2를 각 하나씩 1을 더해
+    # res = [2, 3, 3] 으로 만들어주면 된다.
+    num, rest = divmod(s, n)
+    res = [num] * n
 
-    for _ in range(n):
-        answer.append(s//n)
+    for i in range(rest):
+        res[i] += 1
 
-    indexs = len(answer)-1
+    res.sort()
 
-    for i in range(s - sum(answer)):
-        answer[indexs] += 1
-        indexs -= 1
+    return res
+    
 
-    return answer
+n, s = 2, 9
+print(solution(n, s))
+
