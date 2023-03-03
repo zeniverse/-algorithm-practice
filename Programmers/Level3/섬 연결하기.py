b030dff4cf5322d94585e6d64eaf021890f8a234
origin <0219alice@gmail.com>
2023-03-03 10:20:09 +0900

    [programmers] 섬 연결하기(1)

---
+++ Programmers/Level3/섬 연결하기.py
@@ -0,0 +1,37 @@
+
+
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+
+def solution(n, costs):
+    parent = [0] * n
+
+    for i in range(n):
+        parent[i] = i
+
+    res = 0
+    costs = sorted(costs, key= lambda x: x[2])
+
+    for a, b, cost in costs:
+        if find_parent(parent, a) != find_parent(parent, b):
+            union_parent(parent, a, b)
+            res += cost
+
+    return res
+
+n = 4
+costs = [[0,1,1],[0,2,2],[1,2,5],[1,3,1],[2,3,8]]
+print(solution(n, costs))
