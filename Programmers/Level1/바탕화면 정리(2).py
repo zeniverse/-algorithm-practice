0c81afa822783ece4097065ee465c5804c4d6bf9
zeniverse <0219alice@gmail.com>
2023-03-22 09:50:43 +0900

    [programmers] 바탕화면 정리(2)

---
+++ Programmers/Level1/바탕화면 정리(2).py
@@ -0,0 +1,15 @@
+
+def solution(wallpaper):
+    a, b = [], []
+
+    for i in range(len(wallpaper)):
+        for j in range(len(wallpaper[i])):
+            if wallpaper[i][j] == '#':
+                a.append(i)
+                b.append(j)
+
+    return [min(a), min(b), max(a) + 1, max(b) + 1]
+
+wallpaper = ["..", "#."]
+
+print(solution(wallpaper))
