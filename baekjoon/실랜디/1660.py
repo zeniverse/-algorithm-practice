dca06be7a45412ef6e0821469b9fabf274c58195
zeniverse <0219alice@gmail.com>
2023-07-27 21:56:02 +0900

    [baekjoon] 캡틴 이다솜

---
+++ baekjoon/실랜디/1660.py
@@ -0,0 +1,23 @@
+from sys import stdin
+
+n = int(stdin.readline())
+arr = []
+num = 0
+i = 1
+
+while n > num:
+    num += (i * (i + 1)) // 2
+    arr.append(num)
+    i += 1
+
+dp = [int(1e9) for i in range(n + 1)]
+for i in range(1, n + 1):
+    for num in arr:
+        if num == i:
+            dp[i] = 1
+            break
+        elif num > i:
+            break
+        dp[i] = min(dp[i], 1 + dp[i - num])
+
+print(dp[n])
