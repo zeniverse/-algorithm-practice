bc028c1b1f23d653aca14ff6359ce192ac2f6901
zeniverse <0219alice@gmail.com>
2023-10-31 23:28:32 +0900

    [baekjoon] Base Conversion

---
+++ baekjoon/실랜디/11576.py
@@ -0,0 +1,19 @@
+import math # pow라는 n제곱을 쉽게 해주는 내장함수 이용을 위함
+
+a, b = map(int, input().split())    # a진법의 수를 b진법으로
+n = int(input())    # n자리수인 a진법 수
+lists = list(map(int, input().split()))
+ten = 0	# 10진법으로 만든 수
+result = []
+fin = ''
+
+for i in range(n):
+    ten += int(lists[i] * math.pow(a, n - i - 1))
+
+while ten:
+    nam = ten % b
+    result.append(str(nam))
+    ten = ten // b
+
+result = result[::-1]
+print(*result)
