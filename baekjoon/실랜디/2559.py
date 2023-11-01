cb0a0f3c403255cae16acb84f6252f17e5d13cca
zeniverse <0219alice@gmail.com>
2023-11-01 10:59:39 +0900

    [baekjoon] 수열

---
+++ baekjoon/실랜디/2559.py
@@ -0,0 +1,10 @@
+N, K = map(int, input().split())
+arr = list(map(int, input().split()))
+
+tmp = sum(arr[:K])
+result = tmp
+for i in range(K, N):
+    tmp += arr[i] - arr[i-K]
+    result = max(result, tmp)
+
+print(result)
