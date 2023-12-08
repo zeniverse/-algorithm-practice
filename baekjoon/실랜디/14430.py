96306b658a0d12ddf572ee0633cd6ee050170508
zeniverse <0219alice@gmail.com>
2023-12-08 22:48:36 +0900

    [baekjoon] 자원 캐기

---
+++ baekjoon/실랜디/14430.py
@@ -0,0 +1,18 @@
+n, m = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(n)]
+dp = [[0]*m for _ in range(n)]
+dp[0][0] = arr[0][0]
+
+for i in range(n):
+    for j in range(m):
+        if i == 0 and j == 0:
+            continue
+        elif i > 0 and j == 0:
+            dp[i][j] = dp[i-1][j]
+        elif i == 0 and j > 0:
+            dp[i][j] = dp[i][j-1]
+        else:
+            dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+        if arr[i][j] == 1:
+            dp[i][j] += 1
+print(max(map(max, dp)))
