b97f59ea225996a839749d9195225bc47b438d77
zeniverse <0219alice@gmail.com>
2024-02-02 00:30:18 +0900

    [programmers] 영어가 싫어요(2)

---
+++ Programmers/Level0/영어가 싫어요(2).py
@@ -0,0 +1,12 @@
+
+numbers = "onefourzerosixseven"
+
+def solution(numbers):
+
+    for num, eng in enumerate(["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]):
+        numbers = numbers.replace(eng, str(num))
+    
+    return int(numbers)
+
+
+print(solution(numbers))
