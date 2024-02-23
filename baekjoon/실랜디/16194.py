0a986b4a67ea740a8f1fddc5e349b38b064fc707
zeniverse <0219alice@gmail.com>
2024-02-23 11:56:42 +0900

    [baekjoon] 카드 구매하기 2

---
+++ baekjoon/실랜디/16194.py
@@ -0,0 +1,12 @@
+n = int(input())
+pack = [0] + list(map(int, input().split()))
+dp = [0] * (n + 1)
+
+for i in range(1, n + 1):
+    for j in range(1, i + 1):
+        if dp[i] == 0:
+            dp[i] = dp[i - j] + pack[j]
+        else:
+            dp[i] = min(dp[i], dp[i - j] + pack[j])
+
+print(dp[n])
