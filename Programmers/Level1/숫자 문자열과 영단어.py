1b039d0b6883d886946a17d77e6f56563647551d
zeniverse <0219alice@gmail.com>
2024-06-14 15:32:21 +0900

    [programmers] 숫자 문자열과 영단어

---
+++ Programmers/Level1/숫자 문자열과 영단어.py
@@ -0,0 +1,11 @@
+s = "one4seveneight"
+
+def solution(s):
+    dic = {"zero":"0", "one":"1", "two":"2", "three":"3", "four":"4", "five":"5", "six":"6", "seven":"7", "eight":"8", "nine":"9"}
+    
+    for e, n in dic.items():
+        if e in s:
+            s = s.replace(e, n)
+    return int(s)
+
+print(solution(s))
