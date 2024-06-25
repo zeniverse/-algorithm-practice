d7b13216880c1a3b1a7dd1bf2650c932e292b4ec
zeniverse <0219alice@gmail.com>
2024-06-26 00:20:57 +0900

    [programmers] 2016(2)

---
+++ Programmers/Level1/2016(2).py
@@ -0,0 +1,10 @@
+
+a = 5
+b = 24
+
+def solution(a, b):
+    month = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    day = ['FRI', 'SAT', 'SUN', 'MON', 'TUE', 'WED', 'THU']
+    return day[(sum(month[:a - 1]) + b - 1) % 7]
+
+print(solution(a, b))
