562d20cb08e0f18835eb3fd1b0e1a95227df02a7
zeniverse <0219alice@gmail.com>
2024-07-29 18:30:08 +0900

    [programmers] 즐 서는 방법(2)

---
+++ Programmers/Level2/즐 서는 방법(2).py
@@ -0,0 +1,19 @@
+import math
+
+n = 3
+k = 5
+
+def solution(n, k):
+    answer = []
+    arr = [i for i in range(1, n + 1)]
+
+    while arr:
+        a = (k - 1) // math.factorial(n - 1)
+        answer.append(arr.pop(a))
+
+        k = k % math.factorial(n - 1)
+        n -= 1
+
+    return answer
+
+print(solution(n, k))
