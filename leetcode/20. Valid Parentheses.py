aedc494fcd11484de67bfdb88116df99b5bbd481
zeniverse <0219alice@gmail.com>
2024-10-28 20:37:50 +0900

    [leetcode] 20. valid parentheses

---
+++ leetcode/20. Valid Parentheses.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        for i in s:
+            if i == '(':
+                stack.apend(')')
+            elif i == '[':
+                stack.append(']')
+            elif i == '{':
+                stack.append('}')
+            elif not stack or stack.pop() != i:
+                return False
+        return not stack
